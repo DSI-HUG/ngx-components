96658e19033d40f43601b1addd9d2a4c60469beb
Badisi <badisi84@gmail.com>
2024-04-25 09:44:50 +0200

    feat(demo-app): add angular material

--- projects/demo-app/src/main.ts
+++ projects/demo-app/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -8,5 +9,6 @@ if (environment.production) {
     enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
-    .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [provideAnimations()]
+}).catch(err => console.error(err));
