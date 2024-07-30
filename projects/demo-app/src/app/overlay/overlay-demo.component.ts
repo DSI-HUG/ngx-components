03f114ea91c066b5ad67ea2d089a91dec3b19735
rfrt <remi.frontiere@hug.ch>
2024-07-30 11:44:12 +0200

    fix: overlay angular15

--- projects/demo-app/src/app/overlay/overlay-demo.component.ts
+++ projects/demo-app/src/app/overlay/overlay-demo.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgxOverlayComponent } from '@hug/ngx-overlay';
 
