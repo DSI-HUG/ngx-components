e0577b59bfcd435b564c941f5bea37c765905fe6
dgio <dgio.git@fake>
2025-02-11 18:13:32 +0100

    fix(datepicker): repare manuel entry

--- projects/story-book/src/date-time-picker/date-time-picker.stories.ts
+++ projects/story-book/src/date-time-picker/date-time-picker.stories.ts
@@ -1,19 +1,37 @@
 import { registerLocaleData } from '@angular/common';
 import localeFrExtra from '@angular/common/locales/extra/fr-CH';
 import localeFr from '@angular/common/locales/fr-CH';
-import { FormsModule } from '@angular/forms';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, NativeDateAdapter } from '@angular/material/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, NativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { DateFnsAdapter } from '@angular/material-date-fns-adapter';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
+import { frCH } from 'date-fns/locale';
 
+import { NgxDatepickerButtonsComponent } from '../../../date-picker/src/datepicker-buttons/datepicker-buttons.component';
+import { NgxDatepickerMaskDirective } from '../../../date-picker/src/datepicker-mask/datepicker-mask.directive';
 import { NgxDatepickerWithTimeComponent } from '../../../date-picker/src/datepicker-with-time/datepicker-with-time.component';
 import { NgxNumericStepperComponent } from '../../../numeric-stepper/src/numeric-stepper.component';
 
 registerLocaleData(localeFr, 'fr-CH', localeFrExtra);
+
+export class DejaDateAdapter extends DateFnsAdapter {
+    public override sameDate(first: Date | null, second: Date | null): boolean {
+        return first?.getTime() === second?.getTime();
+    }
+
+    public override compareDate(first: Date, second: Date): number {
+        return first && second ? first.getTime() - second.getTime() : 0;
+    }
+
+    public override getFirstDayOfWeek(): number {
+        return 1;
+    }
+}
 
 export class CustomDateAdapter extends NativeDateAdapter {
     public override format(date: Date, _displayFormat: string): string {
@@ -29,18 +47,33 @@ const meta: Meta<NgxDatepickerWithTimeComponent> = {
         moduleMetadata({
             imports: [
                 FormsModule,
+                ReactiveFormsModule,
                 NoopAnimationsModule,
-                MatDatepickerModule,
                 MatIconModule,
                 MatInputModule,
                 MatFormFieldModule,
+                // Material Datepicker
+                MatDatepickerModule,
+                NgxDatepickerMaskDirective,
+                NgxDatepickerButtonsComponent,
                 NgxNumericStepperComponent
             ],
             providers: [
-                { provide: DateAdapter, useClass: CustomDateAdapter },
+                { provide: MAT_DATE_LOCALE, useValue: frCH },
+                { provide: DateAdapter, useClass: DejaDateAdapter },
                 {
                     provide: MAT_DATE_FORMATS,
-                    useValue: MAT_NATIVE_DATE_FORMATS
+                    useValue: {
+                        parse: {
+                            dateInput: 'dd.MM.yyyy HH:mm'
+                        },
+                        display: {
+                            dateInput: 'dd.MM.yyyy HH:mm',
+                            monthYearLabel: 'MMM yyyy',
+                            dateA11yLabel: 'LL',
+                            monthYearA11yLabel: 'MMMM yyyy'
+                        }
+                    }
                 }
             ]
         })
@@ -62,18 +95,19 @@ export const standard: Story = {
     render: args => ({
         props: {
             ...args,
-            value: new Date(),
+            control: new FormControl(new Date()),
             appearance: 'outline'
         },
         template: `
             <mat-form-field [appearance]="appearance">
                 <mat-label>DatetimePicker</mat-label>
-                <input matInput [(ngModel)]="value" [matDatepicker]="picker" />
-                <mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle>
+                <input #input="matInput" matInput type="text" [formControl]="control" [matDatepicker]="picker" dateTimeFormat />
+                <ngx-datepicker-buttons matSuffix [forPicker]="picker" [forInput]="input"></ngx-datepicker-buttons>
                 <mat-datepicker #picker>
                     <ngx-datepicker-with-time></ngx-datepicker-with-time>
                 </mat-datepicker>
             </mat-form-field>
+            <pre>{{ control.value }}</pre>
           `
     })
 };
