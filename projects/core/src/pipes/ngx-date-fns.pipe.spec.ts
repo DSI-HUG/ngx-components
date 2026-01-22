eda23fc65e53543727b2690ff4dd3bb13bf6c378
damien-guillermet <damien.guillermet@gmail.com>
2026-01-22 12:10:31 +0100

    test(NgxDateFnsPipe): update test (#NGXCPTS-43)

--- projects/core/src/pipes/ngx-date-fns.pipe.spec.ts
+++ projects/core/src/pipes/ngx-date-fns.pipe.spec.ts
@@ -39,7 +39,7 @@ describe('NgxDateFnsPipe', () => {
         expect(pipe.transform(date, 'PP')).toEqual('31 déc. 2025');
         expect(pipe.transform(date, 'Pp')).toEqual('31.12.2025, 17:46');
         expect(pipe.transform(date, 'PPPp')).toEqual('31 décembre 2025 à 17:46');
-        expect(pipe.transform(date, 'PPPPpppp')).toEqual('mercredi 31 décembre 2025 à 17:46:39 GMT+01:00');
+        expect(pipe.transform(date, 'PPPPpp')).toEqual('mercredi 31 décembre 2025 à 17:46:39');
     });
 
 });
