0fa772bbc91e3e6e163c9b9c4e876323221cb6b3
Esther Layat <esther@layat.eu>
2026-03-23 08:38:09 +0100

    fix(layout): add exports for column-bar and panel-bar #DESIGNSYS-104

--- projects/layout/src/index.ts
+++ projects/layout/src/index.ts
@@ -1,9 +1,11 @@
 export * from './actions-group';
 export * from './app-bar';
+export * from './column-bar';
 export * from './filters-group';
 export * from './layout.component';
 export * from './main-bar';
 export * from './panel';
+export * from './panel-bar';
 export * from './providers';
 export * from './search-bar-container';
 
