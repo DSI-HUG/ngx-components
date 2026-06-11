2d931b23430519780d21dc06bd104120ead2d9ce
Guillaume Marcel <guillaume.marcel>
2026-06-11 11:00:50 +0200

    chore(NGXCPTS-44): remove zone from input autosize directive

--- projects/core/src/directives/input-autosize.directive.ts
+++ projects/core/src/directives/input-autosize.directive.ts
@@ -1,7 +1,7 @@
-import { DestroyRef, Directive, ElementRef, inject, NgZone, OnInit } from '@angular/core';
+import { DestroyRef, Directive, ElementRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatInput } from '@angular/material/input';
-import { debounceTime, fromEvent, mergeWith, of, startWith, tap } from 'rxjs';
+import { debounceTime, fromEvent, mergeWith, of, startWith } from 'rxjs';
 
 @Directive({
     selector: '[ngx-input-autosize][matInput], [ngx-input-autosize] [matInput]',
@@ -9,29 +9,25 @@ import { debounceTime, fromEvent, mergeWith, of, startWith, tap } from 'rxjs';
     standalone: true
 })
 export class NgxInputAutosizeDirective implements OnInit {
-    private elementRef = inject<ElementRef<HTMLInputElement>>(ElementRef);
-    private ngZone = inject(NgZone);
-    private matInput = inject(MatInput);
-    private destroyRef = inject(DestroyRef);
+    private readonly elementRef = inject<ElementRef<HTMLInputElement>>(ElementRef);
+    private readonly matInput = inject(MatInput);
+    private readonly destroyRef = inject(DestroyRef);
 
     public ngOnInit(): void {
         const inputElement = this.elementRef.nativeElement;
         const valueChanges$ = this.matInput?.ngControl?.valueChanges ?? of(null);
 
-        this.ngZone.runOutsideAngular(() => {
-            fromEvent<Event>(inputElement, 'input').pipe(
-                tap(x => console.log(x)),
-                mergeWith(fromEvent<Event>(inputElement, 'paste'), valueChanges$),
-                startWith(inputElement.value),
-                debounceTime(5),
-                takeUntilDestroyed(this.destroyRef)
-            ).subscribe(() => {
-                const nbChar = inputElement.value?.length;
+        fromEvent<Event>(inputElement, 'input').pipe(
+            mergeWith(fromEvent<Event>(inputElement, 'paste'), valueChanges$),
+            startWith(inputElement.value),
+            debounceTime(5),
+            takeUntilDestroyed(this.destroyRef)
+        ).subscribe(() => {
+            const nbChar = inputElement.value?.length;
 
-                // Forced to have a fallback value as the "style" is higher than class/usual style in the hierarchy
-                inputElement.style.width = nbChar ? `${nbChar}ch` : '1ch';
-                inputElement.style.maxWidth = '100%';
-            });
+            // Forced to have a fallback value as the "style" is higher than class/usual style in the hierarchy
+            inputElement.style.width = nbChar ? `${nbChar}ch` : '1ch';
+            inputElement.style.maxWidth = '100%';
         });
     }
 }
